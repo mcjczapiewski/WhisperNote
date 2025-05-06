1258aa0da3ceeda6739ca8536a06b4b15b8d39bd
Maciej Czapiewski <mcjczapiewski@gmail.com>
2025-05-06 19:50:57 +0200

    Fix TextDocument redeclaration by moving it to a separate file

--- WhisperNote/SummaryView.swift
+++ WhisperNote/SummaryView.swift
@@ -264,26 +264,4 @@ struct SummaryView: View {
     }
 }
 
-// Document class for exporting text files
-struct TextDocument: FileDocument {
-    static var readableContentTypes: [UTType] { [.plainText] }
 
-    var text: String
-
-    init(initialText: String = "") {
-        text = initialText
-    }
-
-    init(configuration: ReadConfiguration) throws {
-        if let data = configuration.file.regularFileContents {
-            text = String(data: data, encoding: .utf8) ?? ""
-        } else {
-            throw CocoaError(.fileReadCorruptFile)
-        }
-    }
-
-    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        let data = text.data(using: .utf8)!
-        return FileWrapper(regularFileWithContents: data)
-    }
-}
--- WhisperNote/TranscriptView.swift
+++ WhisperNote/TranscriptView.swift
@@ -253,26 +253,4 @@ struct TranscriptView: View {
     }
 }
 
-// Document class for exporting text files
-struct TextDocument: FileDocument {
-    static var readableContentTypes: [UTType] { [.plainText] }
 
-    var text: String
-
-    init(initialText: String = "") {
-        text = initialText
-    }
-
-    init(configuration: ReadConfiguration) throws {
-        if let data = configuration.file.regularFileContents {
-            text = String(data: data, encoding: .utf8) ?? ""
-        } else {
-            throw CocoaError(.fileReadCorruptFile)
-        }
-    }
-
-    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        let data = text.data(using: .utf8)!
-        return FileWrapper(regularFileWithContents: data)
-    }
-}
